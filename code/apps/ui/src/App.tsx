444a421c4ba6623a37de97f11f5dc6db1855fb74
ivanzaida <lurkmorr228@gmail.com>
2023-04-26 01:14:50 +0300

    WIP 1

--- code/apps/ui/src/App.tsx
+++ code/apps/ui/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, FC } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 const App: FC = () => {
@@ -9,9 +8,6 @@ const App: FC = () => {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
